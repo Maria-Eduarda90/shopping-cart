50f14e71c5e4cb6cfdf88f928550d1cd93eef6d9
Mary Skywalker <snowdennaruto90@gmail.com>
2023-04-20 17:02:07 -0400

    function to add quantity of item and calculation to add

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var productValue = [25.00, 20.00];
 var qtd = [0, 0];
 
 function AddItem(item){
+
+    var amount = document.getElementById("amount" + item);
+    var total = document.getElementById("total" + item);
+    
+    qtd[item] += 1;
+    totalAmount[item] = Number.parseFloat(productValue[item]) * qtd[item];
+    amount.innerHTML = qtd[item];
+    total.innerHTML = totalAmount[item].toFixed(2);
     
 }
 
