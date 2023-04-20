5494f03503a9164c2af86f72fbdf079e8731ff3e
Mary Skywalker <snowdennaruto90@gmail.com>
2023-04-20 16:57:25 -0400

    javascript structure

---
+++ script.js
@@ -0,0 +1,15 @@
+var totalAmount = [0, 0];
+var productValue = [25.00, 20.00];
+var qtd = [0, 0];
+
+function AddItem(item){
+    
+}
+
+function removeItem(item){
+    
+}
+
+function purchaseValue(){
+    
+}
